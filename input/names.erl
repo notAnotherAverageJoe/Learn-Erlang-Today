be3ee6ca859fedfcb4fe24ccf06c2a74c14ce762
joseph skokan <joeskokan20@gmail.com>
2025-02-13 23:45:45 -0500

    ages

--- input/names.erl
+++ input/names.erl
@@ -8,3 +8,8 @@ get_name() ->
     io:format("Enter your Last Name: ~n"),
     LastName = string:trim(io:get_line("> ")),
     io:format("Users Name: ~s ~s~n", [FirstName, LastName]).
+
+get_age() ->
+    io:format("Enter your Age: ~n"),
+    Age = string:trim(io:get_line("> ")),
+    io:format("Users Age: " ++ Age ++ ".~n").
