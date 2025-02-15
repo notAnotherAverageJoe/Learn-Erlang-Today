0ccbc76092679eedf82a9f8513c3b8c1853f6024
joseph skokan <joeskokan20@gmail.com>
2025-02-15 00:10:46 -0500

    more filter

--- filter/pizza.erl
+++ filter/pizza.erl
@@ -1,5 +1,5 @@
 -module(pizza).
--export([pizza/0]).
+-export([pizza/0, number_of_orders/2]).
 
 pizza() ->
     Orders = [
@@ -18,4 +18,27 @@ pizza() ->
             Order
         )
     end,
-    FilterOrdersForPizza(Orders).
+
+    FilterOrdersForChicken = fun(Order) ->
+        lists:filter(
+            fun({Item, _Customer}) ->
+                Item == chicken
+            end,
+            Order
+        )
+    end,
+    FilterOrdersForChicken = fun(Order, CustomItem) ->
+        lists:filter(
+            fun({Item, _Customer}) ->
+                Item == CustomItem
+            end,
+            Order
+        )
+    end,
+    io:format("Total Orders: ~p~n", [number_of_orders(Orders, 0)]),
+
+    io:format("Pizza Orders: ~p~n", [FilterOrdersForPizza(Orders)]),
+    io:format("Chicken Orders: ~p~n", [FilterOrdersForChicken(Orders)]).
+
+number_of_orders([], Acc) -> Acc;
+number_of_orders([_Hd | Tl], Acc) -> number_of_orders(Tl, Acc + 1).
