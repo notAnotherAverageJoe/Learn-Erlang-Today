04ac3bd33a1315d36792726c229b05777693b979
joseph skokan <joeskokan20@gmail.com>
2025-02-15 23:31:28 -0500

    hero fold

--- folds/hero.erl
+++ folds/hero.erl
@@ -1,2 +1,20 @@
 -module(hero).
--export([]).
+-export([adventure/0]).
+
+adventure() ->
+    Hero = [
+        {hero, 100, 1},
+        {enemy1, 50, 1},
+        {enemy2, 50, 1},
+        {enemy3, 70, 1},
+        {enemy4, 80, 1}
+    ],
+
+    HeroLevel = lists:foldl(
+        fun({_Name, Level, Amount}, Power) ->
+            (Level * Amount) + Power
+        end,
+        0,
+        Hero
+    ),
+    io:format("Hero's Power -> ~p~n", [HeroLevel]).
