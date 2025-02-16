4f75acad722a7693ef2e309a54f07139d64d3deb
joseph skokan <joeskokan20@gmail.com>
2025-02-15 23:11:13 -0500

    first fold

--- folds/cereal.erl
+++ folds/cereal.erl
@@ -1,3 +1,21 @@
 -module(cereal).
--export([]).
+-export([breakfast/0]).
 -author("Joe").
+
+breakfast() ->
+    Cereals = [
+        {trix, 5.99, 2},
+        {raisin, 5.99, 2},
+        {pebbles, 5.99, 2},
+        {cocopuffs, 5.99, 2},
+        {luckycharms, 5.99, 2},
+        {frostedwheaties, 5.99, 2}
+    ],
+
+    lists:foldl(
+        fun({_Cereal, Price, Quantity}, Sum) ->
+            (Price * Quantity) + Sum
+        end,
+        0,
+        Cereals
+    ).
