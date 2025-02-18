c7c45437f6884555993774915e0e58258f8c7d68
joseph skokan <joeskokan20@gmail.com>
2025-02-18 00:31:05 -0500

    small addition

--- textAdventure/heroes.erl
+++ textAdventure/heroes.erl
@@ -5,9 +5,11 @@ start_adventurer() ->
     spawn(?MODULE, hero_status, [{10, 10, 10}]).
 
 loop(Pid)->
-    io:format("Choose an action~n1. Train strength~n2.Train Magic~n3.Train Stamina"),
+    io:format("Choose an action~n1. Train strength~n2.Train Magic~n3.Train Stamina~n or 'q' to quit"),
     Choice = string:trim(io:get_line()),
     case Choice of
+        "q" ->
+            io:format("Goodbye!~n");
         "1"->
             Pid ! {strength_training, 10, self()},
             receive 
@@ -20,3 +22,12 @@ loop(Pid)->
             receive
                 {up_magic, NewMagic} ->
                     io:format("Magic power increased to: ~p~n",[NewMagic])
+            end,
+            loop(Pid);
+        "3" ->
+            Pid ! {stamina_training, 10, self()},
+            receive
+                {up_stamina, NewStamina}->
+                    io:format("New Stamina level: ~p~n",[NewStamina])
+            end,
+            loop(Pid)
