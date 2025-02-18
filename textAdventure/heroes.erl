9563471771250f258efb9b53472620f25ce8ac1a
joseph skokan <joeskokan20@gmail.com>
2025-02-18 00:32:37 -0500

    main loop

--- textAdventure/heroes.erl
+++ textAdventure/heroes.erl
@@ -4,30 +4,36 @@
 start_adventurer() ->
     spawn(?MODULE, hero_status, [{10, 10, 10}]).
 
-loop(Pid)->
-    io:format("Choose an action~n1. Train strength~n2.Train Magic~n3.Train Stamina~n or 'q' to quit"),
+loop(Pid) ->
+    io:format(
+        "Choose an action~n1. Train strength~n2.Train Magic~n3.Train Stamina~n or 'q' to quit"
+    ),
     Choice = string:trim(io:get_line()),
     case Choice of
         "q" ->
             io:format("Goodbye!~n");
-        "1"->
+        "1" ->
             Pid ! {strength_training, 10, self()},
-            receive 
-                {up_strength, NewStrength}->
-                    io:format("Strength level increased to: ~p~n",[NewStrength])
+            receive
+                {up_strength, NewStrength} ->
+                    io:format("Strength level increased to: ~p~n", [NewStrength])
             end,
             loop(Pid);
         "2" ->
             Pid ! {magic_training, 10, self()},
             receive
                 {up_magic, NewMagic} ->
-                    io:format("Magic power increased to: ~p~n",[NewMagic])
+                    io:format("Magic power increased to: ~p~n", [NewMagic])
             end,
             loop(Pid);
         "3" ->
             Pid ! {stamina_training, 10, self()},
             receive
-                {up_stamina, NewStamina}->
-                    io:format("New Stamina level: ~p~n",[NewStamina])
+                {up_stamina, NewStamina} ->
+                    io:format("New Stamina level: ~p~n", [NewStamina])
             end,
+            loop(Pid);
+        _ ->
+            io:format("Invalid option try again~n"),
             loop(Pid)
+    end.
