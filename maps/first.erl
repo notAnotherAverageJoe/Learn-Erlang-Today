c56f790a7c7b7e7ec98745fd5c7d1ccad93125ee
joseph skokan <joeskokan20@gmail.com>
2025-03-10 16:23:32 -0400

    more maps

--- maps/first.erl
+++ maps/first.erl
@@ -5,3 +5,9 @@ start() ->
     OtherMap = #{language => erlang},
     Defaults = maps:merge(DefaultMap, OtherMap),
     io:format("~p~n", [Defaults]).
+
+numbers_joined() ->
+    FirstHalf = #{nums => [1, 5, 6, 7]},
+    SecondHalf = #{nums2 => [2, 3, 4]},
+    Default = maps:merge(FirstHalf, SecondHalf),
+    io:format("~p~n").
