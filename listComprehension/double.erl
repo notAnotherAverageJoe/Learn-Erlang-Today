f6fb61528ea539f807e2a95e06d187ede261a6d4
joseph skokan <joeskokan20@gmail.com>
2025-03-21 01:39:21 -0400

    added quad

--- listComprehension/double.erl
+++ listComprehension/double.erl
@@ -3,7 +3,9 @@
 start() ->
     doubles(),
     find_evens(),
-    find_odds().
+    find_odds(),
+    quads(),
+    square().
 
 doubles() ->
     Doubles = [X * 2 || X <- [1, 2, 3, 4, 5]],
@@ -16,3 +18,11 @@ find_evens() ->
 find_odds() ->
     Odds = [X || X <- [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], X rem 2 /= 0],
     io:format("Odds: ~p~n", [Odds]).
+
+quads() ->
+    Quad = [X * 4 || X <- [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]],
+    io:format("Quad List: ~p~n", [Quad]).
+
+square() ->
+    Squares = [X || X <- [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]],
+    io:format("Square List: ~p~n", [Squares]).
